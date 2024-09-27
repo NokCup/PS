1802c10685c97eba151074a33c6ea41d6d7f8f9e
NokCup <nokkup84@gmail.com>
2024-09-27 16:01:46 +0900

    update

---
+++ 백준/Bronze/2588. 곱셈 (문제 해설O)/곱셈.py
@@ -0,0 +1,5 @@
+# 내 코드
+a1 = int(input())
+a2 = int(input())
+
+print(a1 * (a2 % 10), a1 * ((a2 % 100) // 10), a1 * (a2 // 100), a1 * a2, sep="\n")
--- 백준/Bronze/2588. 곱셈/곱셈.py
+++
@@ -1,5 +0,0 @@
-# 내 코드
-a1 = int(input())
-a2 = int(input())
-
-print(a1 * (a2 % 10), a1 * ((a2 % 100) // 10), a1 * (a2 // 100), a1 * a2, sep="\n")
