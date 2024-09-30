fbec2d1d4b688a88d8e881d898bf84f8a72b2b6d
NokCup <nokkup84@gmail.com>
2024-09-30 16:30:59 +0900

    update

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-
-for i in range(-n + 1, n):      # -4, -3, -2, -1, 0, 1, 2, 3, 4
-    print(' ' * ((n - 1) - abs(i)) + '*' * (2*(abs(i) + 1) - 1))
--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++
@@ -1,9 +0,0 @@
-n = 1000 - int(input()) # 거스름돈
-coins = [500, 100, 50, 10, 5, 1]
-
-count = 0
-for coin in coins:
-    count += n // coin
-    n %= coin
-
-print(count)
