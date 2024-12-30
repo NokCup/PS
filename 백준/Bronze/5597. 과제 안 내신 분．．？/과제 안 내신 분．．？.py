154e617a0e9147162c23d6439350ce44dcdcc00b
NokCup <nokkup84@gmail.com>
2024-12-30 17:21:30 +0900

    Update 과제 안 내신 분．．？.py
    데이터 입력과 출력 부분 업데이트

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,11 @@
-data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+# 1부터 30까지의 데이터 정보
+data = []
+for i in range(1, 31):
+    data.append(i)
 
+# 입력한 수를 데이터에서 제거
 for _ in range(28):
     data.remove(int(input()))
 
-data.sort()
-print(data[0])
-print(data[1])
+# 데이터 출력
+print(*data, sep='\n')
